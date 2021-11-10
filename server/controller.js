3528fa0b3a666ecc76917972cbbdd2a7650a9492
Tamara Chance <tamara.n.chance@gmail.com>
2021-11-10 16:05:42 -0600

    part 1-7 completed

--- server/controller.js
+++ server/controller.js
@@ -1,6 +1,34 @@
+require('dotenv').config()
+const { CONNECTION_STRING } = process.env
+const Sequelize = require('sequelize')
+
+let sequelize = new Sequelize (CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
+
 let nextEmp = 5
 
 module.exports = {
+    getAllClients: (req, res) => {
+        sequelize.query(`
+        SELECT * FROM cc_clients c
+        JOIN cc_users u On u.user_id = c.user_id
+        `)
+            .then(dbRes => res.status(200).send(dbRes[0]))
+    },
+    getPendingAppointments: (req, res) => {
+        sequelize.query(`
+        SELECT * FROM cc_appointments a
+        WHERE approved = false
+        ORDER BY date DESC
+        `)
+            .then(dbRes => res.status(200).send(dbRes[0]))
+    },
     getUpcomingAppointments: (req, res) => {
         sequelize.query(`select a.appt_id, a.date, a.service_type, a.approved, a.completed, u.first_name, u.last_name 
         from cc_appointments a
@@ -12,14 +40,28 @@ module.exports = {
             .then(dbRes => res.status(200).send(dbRes[0]))
             .catch(err => console.log(err))
     },
-
+    getPastAppointments: (req, res) => {
+        sequelize.query(`
+        SELECT a.appt_id, a.date, a.service_type, a.notes, a.approved, a.completed, u.first_name, u.last_name 
+        FROM cc_appointments a
+        JOIN cc_emp_appts ea ON a.appt_id = ea.appt_id
+        JOIN cc_employees e ON e.emp_id = ea.emp_id
+        JOIN cc_users u ON e.user_id = u.user_id
+        WHERE a.approved = true AND a.completed = true
+        ORDER BY a.date DESC;
+        `)
+            .then(dbRes => res.status(200).send(dbRes[0]))
+    },
     approveAppointment: (req, res) => {
         let {apptId} = req.body
     
-        sequelize.query(`*****YOUR CODE HERE*****
-        
-        insert into cc_emp_appts (emp_id, appt_id)
-        values (${nextEmp}, ${apptId}),
+        sequelize.query(`
+        UPDATE cc_appointments
+        SET approved = true
+        WHERE appt_id = ${apptId}
+
+        INSERT INTO cc_emp_appts (emp_id, appt_id)
+        VALUES (${nextEmp}, ${apptId}),
         (${nextEmp + 1}, ${apptId});
         `)
             .then(dbRes => {
@@ -27,5 +69,14 @@ module.exports = {
                 nextEmp += 2
             })
             .catch(err => console.log(err))
+    },
+    completeAppointment: (req, res) => {
+        let {apptId} = req.body
+        sequelize.query(`
+        UPDATE cc_appointments
+        SET completed = true
+        WHERE appt_id = ${apptId}
+        `)
+            .then(dbRes => res.status(200).send(dbRes[0]))
     }
 }
