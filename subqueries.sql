da4b6e2fd2a544b962c4a665ddb3842fc689b38d
Tamara Chance <tamara.n.chance@gmail.com>
2021-11-10 16:22:08 -0600

    p1&2 done

---
+++ subqueries.sql
@@ -0,0 +1,22 @@
+-- Get all invoices where the unit_price on the invoice_line is greater than $0.99.
+select * 
+from invoice
+where invoice_id in 
+	(select invoice_id from invoice_line
+  where unit_price > .99);
+
+-- Get all playlist tracks where the playlist name is Music.
+select * 
+from playlist_track
+where playlist_id in 
+	(select playlist_id 
+   from playlist
+   where name = 'Music');
+
+-- Get all track names for playlist_id 5.
+
+-- Get all tracks where the genre is Comedy.
+
+-- Get all tracks where the album is Fireball.
+
+-- Get all tracks for the artist Queen ( 2 nested subqueries ).
